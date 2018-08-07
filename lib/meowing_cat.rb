8d7195aadf48c8b8bf52b567a58822632dd72198
Madeline Celis <celiscb@vcu.edu>
2018-08-07 19:03:32 -0400

    complete

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,11 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+  #creates getter and setter functions for what ever you put :variable_name after
+  #be extremely careful you must write attr_accessor not attribute_accessor
+  #attr_writer/ attr_reader
+  attr_accessor :name
+
+  def meow
+    puts 'meow!'
+  end
+end
